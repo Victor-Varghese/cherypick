7f2e2c3d90ef19e0e54233da77d18a4b28547a53
Victor Varghese <Victor.Varghese@Neudesic.com>
2019-05-09 12:13:33 +0530

    fixed 6

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ Fixed issue1
 Fixed issue2
 Fixed isue 4
 Fixed issue 5
+
+Fixing issue 6
 import React, { Component } from 'react'
 import { Platform, NativeModules } from 'react-native'
 import { Route, Switch } from 'react-router'
